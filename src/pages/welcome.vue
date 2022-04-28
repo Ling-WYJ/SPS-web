ff92fe51ffff8e4232481fa3106ad5c32c8705bb
张灏琪 <shirley_z0914@163.com>
2022-04-28 13:01:53 +0800

    新增欢迎页

---
+++ src/pages/welcome.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="welcome-message">欢迎使用社会心理服务平台</div>
+</template>
+
+<script>
+export default {
+  name: "welcome",
+  data() {
+    return {};
+  },
+  components: {},
+  created() {},
+  methods: {},
+};
+</script>
+
+<style>
+.welcome-message {
+  line-height: calc(100vh - 90px)!important;
+  vertical-align: middle!important;
+  text-align: center!important;
+  font-size: 3rem;
+  color:#304156
+}
+</style>
